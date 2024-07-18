e57e14d24673738f334c000e1b5befdb196563f1
kristjan kangro <kristjan.kangro@visma.com>
2024-07-18 21:02:15 +0300

    models, classes etc

---
+++ Decorator/Clip01Demo.cs
@@ -0,0 +1,22 @@
+﻿using Demo.Common;
+
+namespace Demo.Clip01
+{
+    class Clip01Demo : Common.Demo
+    {
+        protected override int ClipNumber { get; } = 1;
+        private readonly Length mm = Length.Millimeter;
+
+        protected override void Implementation()
+        {
+            var book = new Book("Title",
+                new Size(188 * mm, 239 * mm, 28 * mm));
+            var customer = new Buyer();
+            customer.Handle(book);
+
+            var employee = new Dispatcher();
+            employee.Handle(book);
+
+        }
+    }
+}
--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Demo.Clip01;
+
+new Clip01Demo().Run();
