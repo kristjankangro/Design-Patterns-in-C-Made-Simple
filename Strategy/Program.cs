e62fe58a1f80070529571c685414c7559f635e2f
kristjan kangro <kristjan.kangro@visma.com>
2024-09-30 20:01:19 +0300

    strategy start

---
+++ Strategy/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Data.SqlTypes;
+using Demo.Clip01;
+using Strategy;
+using Strategy.Common;
+
+var book = new Book("Design patterns: Elements of reusable OOP Software",
+    new Money( 35, new Currency("USD")));
+Console.WriteLine("Hello, World!");
---
+++ Strategy/TakeTwoOffer.cs
@@ -0,0 +1,18 @@
+﻿using Strategy;
+
+namespace Demo.Clip01
+{
+    public class TakeTwoOffer
+    {
+        private Book First { get; }
+        private Book Second { get; }
+
+        public TakeTwoOffer(Book first, Book second)
+        {
+            this.First = first;
+            this.Second = second;
+        }
+
+        public (Book first, Book second) Apply() => (this.First, this.Second);
+    }
+}
