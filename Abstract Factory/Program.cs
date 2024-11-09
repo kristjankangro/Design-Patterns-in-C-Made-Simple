a60c620ac12abad5313d5e11a5edc7e4253a1976
kristjan kangro <kristjan.kangro@visma.com>
2024-11-09 17:18:23 +0200

    factory method currying

--- Abstract Factory/Program.cs
+++ Abstract Factory/Program.cs
@@ -9,23 +9,35 @@ namespace AbstractFactory
     {
         static void Main(string[] args)
         {
-            var connStr = @"Data Source=localhost;Initial Catalog=DemoDB;User Id=my;Password=name";
-            
-            //supports only localhost and fails 
-            var connStrFailur = @"Data Source=SomeotherServer;Initial Catalog=DemoDB;User Id=my;Password=name";
-            
-            
-            IDataAccess gateway = new FastDataAccess();
+            Use(new CheapDataAccess(), ConnStrFactoryFor("localhost"));
+            Use(new FastDataAccess(), ConnStrFactoryFor("BankServer"));
+            Use(new FastDataAccess(), pwd => $"Data Source=localhost;Initial Catalog=DemoDB;User Id=my;Password={pwd}");
+        }
 
+        private static string CreateConnStr(string server, string password)
+        {
+            return $"Data Source={server};Initial Catalog=DemoDB;User Id=my;Password={password}";
+        }
+
+        private static Func<string, string> ConnStrFactoryFor(string server)
+        {
+            return password => CreateConnStr(server, password);
+        }
+
+        private static string Server = "localhost";
+        private static string Database = "DemoDB";
+        private static string User = "my";
+
+        private static string LocalConnStr(string password) =>
+            $"Data Source={Server};Initial Catalog={Database};User Id={User};Password={password}";
+
+        private static void Use(IDataAccess gateway, Func<string, string> createConnStr)
+        {
+            string password = "123456";
+            var connStr = createConnStr(password);
+            // var connStr = @"Data Source=localhost;Initial Catalog=DemoDB;User Id=my;Password=name";
             new HeadHunter(connStr, gateway).AddEmployee("Kristjan");
-
-            IDataAccess db = new CheapDataAccess();
-            
-            new HeadHunter(connStr, db).AddEmployee("Kristjan");
-            
             Console.WriteLine();
-            Console.Write("Press ENTER to continue . . . ");
-            Console.ReadLine();
         }
     }
 }
