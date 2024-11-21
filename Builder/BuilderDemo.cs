cab670b7576b477739a44fae22805231a3508749
Kristjan <kristjan.kangro@visma.com>
2024-11-21 16:58:41 +0200

    Builder : failing with factory method

---
+++ Builder/BuilderDemo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace BuilderDemo
+{
+    class BuilderDemo : Demo.Common.Demo
+    {
+        private string Escape(string s) => ";' \t".ToCharArray().Any(s.Contains) ? $"\"{s.Replace("\"","\"\"")}\""
+            : s.Contains("\"") ? $"'{s}'"
+            : s;
+        
+        private string CreateConnectionString(string dataSource, int port, string catalog, string username, string password, bool security) =>
+            $"Data Source={Escape(port >= 0 ? $"{dataSource},{port}" : dataSource)};" +
+            $"Initial Catalog={Escape(catalog)};" +
+            (security ? $"; Integrated Security=true" : $"User Id={Escape(username)};" + $"Password={Escape(password)}");
+
+        protected override void Implementation()
+        {
+            //problems many arguments, semicolon in any param can lead to security issue, injection
+            var connString = CreateConnectionString("localhost", 1433, "DemoDB", "my", "na;Initial Catalog=Bank", true);
+
+            Console.WriteLine(connString);
+        }
+    }
+}
---
+++ Builder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Demo.Clip02;
+using Demo.Clip03;
+using Demo.Clip04;
+using Demo.Clip05;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new BuilderDemo.BuilderDemo().Run();
+            // new Clip02Demo().Run();
+            // new Clip03Demo().Run();
+            // new Clip04Demo().Run();
+            // new Clip05Demo().Run();
+
+            Console.WriteLine();
+            Console.Write("Press ENTER to continue . . . ");
+            Console.ReadLine();
+        }
+    }
+}
