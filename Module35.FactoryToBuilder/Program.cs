7575acf9e3aaf39eeceb86557479f7b98d58d1ab
kristjan kangro <kristjan.kangro@visma.com>
2024-11-30 14:01:01 +0200

    module 35 init builder

---
+++ Module35.FactoryToBuilder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Demo.Clip02;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Clip02Demo().Run();
+
+            Console.WriteLine();
+            Console.Write("Press ENTER to continue . . . ");
+            Console.ReadLine();
+        }
+    }
+}
