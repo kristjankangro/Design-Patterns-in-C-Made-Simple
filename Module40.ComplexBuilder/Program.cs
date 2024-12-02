276644b70bddc682a9bee177dcbfaffb9e4f81a2
kristjan kangro <kristjan.kangro@visma.com>
2024-12-02 21:05:46 +0200

    module 40 complex builders init

---
+++ Module40.ComplexBuilder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Demo.Clip01;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Clip01Demo().Run();
+
+            Console.WriteLine();
+            Console.Write("Press ENTER to continue . . . ");
+            Console.ReadLine();
+        }
+    }
+}
