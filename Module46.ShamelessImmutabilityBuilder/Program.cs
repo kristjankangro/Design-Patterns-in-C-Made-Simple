2f1ebe31fb732205946f1b0fc63c3648889e2f70
kristjan kangro <kristjan.kangro@visma.com>
2025-01-04 20:51:51 +0200

    module 46 start

---
+++ Module46.ShamelessImmutabilityBuilder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Demo.Clip07;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Clip07Demo().Run();
+
+            Console.WriteLine();
+            Console.Write("Press ENTER to continue . . . ");
+            Console.ReadLine();
+        }
+    }
+}
